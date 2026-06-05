b8a334c5dd299eb2cb855d57d1cab67197775116
nvtanK22TT <nvtan.k22tt@kontum.udn.vn>
2026-06-05 18:00:45 +0700

    Fix ProtectedRoute user role

--- frontend/src/components/route/ProtectedRoute.js
+++ frontend/src/components/route/ProtectedRoute.js
@@ -16,7 +16,7 @@ const ProtectedRoute = ({ isAdmin, component: Component, ...rest }) => {
                             return <Redirect to='/login' />
                         }
 
-                        if (isAdmin === true && user.role !== 'admin') {
+                        if (isAdmin === true && user?.role !== 'admin') {
                             return <Redirect to="/" />
                         }
 
